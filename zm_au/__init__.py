871642c08a80b9176cc4c875c64158fa6fe05a24
Zeke Marffy <zmarffy@me.com>
2022-02-18 17:33:10 -0500

    version stuff

--- zm_au/__init__.py
+++ zm_au/__init__.py
@@ -2,4 +2,13 @@ from .base_au import BaseAU
 from .pip_github_au import PipGitHubAU
 from .pip_au import PipAU
 
+from zetuptools import PipPackage
+
+package_name = "au"
+
 __version__ = '0.0.0'
+if __version__ == "0.0.0":
+    try:
+        __version__ = PipPackage(package_name).version
+    except FileNotFoundError:
+        pass
