72a0543de4b9f7e1be1a2323fe0241d3338759fd
Zeke Marffy <zmarffy@me.com>
2023-06-05 19:12:56 -0400

    update poetry stuff

--- zm_au/__init__.py
+++ zm_au/__init__.py
@@ -1,14 +1,5 @@
-from .base_au import BaseAU
-from .pip_github_au import PipGitHubAU
-from .pip_au import PipAU
+from .base import BaseAU
+from .pip.github import PipGitHubAU
+from .pip.base import PipAU
 
-from zetuptools import PipPackage
-
-package_name = "au"
-
-__version__ = '0.0.0'
-if __version__ == "0.0.0":
-    try:
-        __version__ = PipPackage(package_name).version
-    except FileNotFoundError:
-        pass
+__version__ = "0.0.0"
