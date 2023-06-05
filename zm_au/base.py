2b0dea91f13616f1272667334354f000ad173ae4
Zeke Marffy <zmarffy@me.com>
2023-06-05 19:13:13 -0400

    update poetry stuff

---
+++ zm_au/base.py
@@ -0,0 +1,80 @@
+import sys
+from pathlib import Path
+from tempfile import TemporaryDirectory
+
+import packaging.version
+import zmtools
+
+
+class UpdateException(Exception):
+    # Base exception class for this module
+    pass
+
+
+class BaseAU:
+    """Base auto-updater class. Will not work as is; its purpose is to be inherited from.
+
+    Attributes:
+        current_version (str): The current version of the package.
+        latest_version (str): The latest version of the package.
+        needs_update (bool): If the package is not the latest version.
+    """
+    def __init__(self) -> None:
+        self.current_version = self._get_current_version()
+        self.latest_version = self._get_latest_version()
+
+    def _get_current_version(self) -> str:  # type: ignore
+        # Override me!
+        # This function must return the current version of the package
+        pass
+
+    def _get_latest_version(self) -> str:  # type: ignore
+        # Override me!
+        # This function must return the latest version of the package
+        pass
+
+    def _download(self) -> Path:  # type: ignore
+        # Override me!
+        # This function must download the package and return the path of the downloaded file
+        pass
+
+    def _update(self, update_file: Path) -> None:
+        # Override me!
+        # This function must install a package whose location is passed via the sole parameter
+        pass
+
+    def update(self, prompt: bool = True) -> None:
+        """Update the package.
+
+        Args:
+            prompt (bool, optional): If True, prompt before updating. Defaults to True.
+        """
+        if self.needs_update and (
+            not prompt
+            or zmtools.y_to_continue(
+                f"A newer version ({self.latest_version}) is available. Would you like to update?"
+            )
+        ):
+            with TemporaryDirectory() as temp_dir:
+                with zmtools.working_directory(temp_dir):
+                    update_file = self._download()
+                    self._update(update_file)
+            print("Exiting as an update has completed")
+            sys.exit()
+
+    @property
+    def currently_installed_version_is_unreleased(self) -> bool:
+        # Override me!
+        # This property should return True if the currently installed version of a package is in development, i.e. an unreleased commit
+        return False
+
+    @property
+    def needs_update(self) -> bool:
+        if self.current_version == "0.0.0":
+            return False
+        elif self.currently_installed_version_is_unreleased:
+            return False
+        else:
+            return packaging.version.parse(
+                self.latest_version
+            ) > packaging.version.parse(self.current_version)
---
+++ zm_au/pip/github.py
@@ -0,0 +1,101 @@
+import importlib
+import json
+import os
+import subprocess
+import sys
+from typing import Optional
+
+import packaging.version
+from install_directives import PipPackage
+
+from ..base import BaseAU, UpdateException
+
+
+class PipGitHubAU(BaseAU):
+    def __init__(
+        self,
+        name: str,
+        github_location: str,
+        check_prerelease: bool = False,
+        dist: str = "whl",
+        silent: bool = False,
+    ):
+        # If silent, redirect output of pip to the null device
+        # Other parameters tell which uploads to look for and where (note that github_location's format is like "zmarffy/au")
+        self._pip_package = PipPackage(name)
+        self.github_location = github_location
+        self.check_prerelease = check_prerelease
+        self.dist = dist
+        self.silent = silent
+        super().__init__()
+
+    @property
+    def _o(self) -> Optional[int]:
+        # Determines what to redirect output to
+        if self.silent:
+            return subprocess.DEVNULL
+        else:
+            return None
+
+    def _get_current_version(self) -> str:
+        return self._pip_package.version
+
+    def _get_latest_version(self) -> str:
+        # Uses `gh api` to find the latest version
+        try:
+            d = json.loads(
+                subprocess.check_output(
+                    ["gh", "api", f"repos/{self.github_location}/releases"]
+                )
+            )
+        except subprocess.CalledProcessError:
+            raise UpdateException(
+                f"Either {self.github_location} doesn't exist or you need to authorize with GitHub (run `gh auth login`) before attempting to check for updates for it"
+            )
+        if not self.check_prerelease:
+            latest = next(x for x in d if not x["prerelease"])
+        else:
+            latest = d[0]
+        return latest["tag_name"]
+
+    def _download(self) -> str:
+        # Uses `gh release download` to download the version found from _get_latest_version (doesn't blindly install the latest version to avoid a super rare race condition)
+        subprocess.run(
+            [
+                "gh",
+                "release",
+                "download",
+                self.latest_version,
+                "-R",
+                self.github_location,
+                "-p",
+                f"*.{self.dist}",
+            ],
+            stdout=self._o,
+        )
+        files = os.listdir()
+        if len(files) != 1:
+            raise ValueError(
+                f"Ambiguous install instructions as multiple files were downloaded. Files: {files}"
+            )
+        return files[0]
+
+    def _update(self, update_file: str):
+        # Installs a file with `pip install`
+        subprocess.run(
+            [sys.executable, "-m", "pip", "install", update_file], stdout=self._o
+        )
+        try:
+            importlib.import_module(f"{self._pip_package.name}.install_directives")
+            has_id = True
+        except ModuleNotFoundError:
+            has_id = False
+        if has_id:
+            subprocess.run(["install-directives", self._pip_package.name, "install"])
+
+    @property
+    def currently_installed_version_is_unreleased(self) -> bool:
+        return (
+            bool(packaging.version.parse(self.current_version).local)
+            or self._pip_package.editable
+        )
